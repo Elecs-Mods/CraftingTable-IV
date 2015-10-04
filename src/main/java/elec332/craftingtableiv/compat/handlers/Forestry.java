35b00dd425abe03babe80a752d64489dce03def9
Elec332 <arnout1998@gmail.com>
2015-10-04 14:02:05 +0200

    Fix forestry compat, closes #11

--- src/main/java/elec332/craftingtableiv/compat/handlers/Forestry.java
+++ src/main/java/elec332/craftingtableiv/compat/handlers/Forestry.java
@@ -2,8 +2,9 @@ package elec332.craftingtableiv.compat.handlers;
 
 import elec332.craftingtableiv.api.AbstractRecipeHandler;
 import elec332.craftingtableiv.compat.AbstractCompatModule;
-import forestry.core.utils.ShapedRecipeCustom;
-import forestry.lepidopterology.MatingRecipe;
+import forestry.core.recipes.ShapedRecipeCustom;
+import forestry.core.recipes.ShapelessRecipeCustom;
+import forestry.lepidopterology.recipes.MatingRecipe;
 
 /**
  * Created by Elec332 on 4-10-2015.
@@ -21,6 +22,12 @@ public class Forestry extends AbstractCompatModule {
             @Override
             public Object[] getIngredients(ShapedRecipeCustom recipe) {
                 return recipe.getIngredients();
+            }
+        });
+        registerHandler(ShapelessRecipeCustom.class, new AbstractRecipeHandler<ShapelessRecipeCustom>() {
+            @Override
+            public Object[] getIngredients(ShapelessRecipeCustom recipe) {
+                return recipe.getIngredients().toArray();
             }
         });
         registerDisabledRecipe(MatingRecipe.class);
