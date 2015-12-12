8dee7df3965068820c1a1bd9980e428097782eb7
Elec332 <arnout1998@gmail.com>
2015-12-12 14:58:10 +0100

    Whoops

--- src/main/java/elec332/craftingtableiv/blocks/BlockCraftingTableIV.java
+++ src/main/java/elec332/craftingtableiv/blocks/BlockCraftingTableIV.java
@@ -1,15 +1,14 @@
 package elec332.craftingtableiv.blocks;
 
+import elec332.core.client.IIconRegistrar;
 import elec332.core.client.model.ElecModelBakery;
 import elec332.core.client.model.ElecQuadBakery;
 import elec332.core.client.model.INoJsonBlock;
-import elec332.core.client.model.RenderingRegistry;
 import elec332.core.client.model.model.IBlockModel;
 import elec332.core.client.model.model.TESRItemModel;
 import elec332.core.client.model.template.ElecTemplateBakery;
 import elec332.craftingtableiv.client.CraftingTableIVRenderer;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.resources.model.IBakedModel;
 import net.minecraft.item.Item;
 import net.minecraft.util.BlockPos;
@@ -58,52 +57,23 @@ public class BlockCraftingTableIV extends BlockTileBase implements INoJsonBlock 
         return 2;
     }
 
-    /**
-     * This method is used when a model is requested to render the block in a world.
-     *
-     * @param state The current BlockState.
-     * @param iba   The IBlockAccess the block is in.
-     * @param pos   The position of the block.
-     * @return The model to render for this block for the given arguments.
-     */
     @Override
     public IBlockModel getBlockModel(IBlockState state, IBlockAccess iba, BlockPos pos) {
         return null;
     }
 
-    /**
-     * This method is used when a model is requested when its not placed, so for an item.
-     *
-     * @param item
-     * @param meta
-     * @return The model to render when the block is not placed.
-     */
     @Override
     public IBakedModel getBlockModel(Item item, int meta) {
         return this.model;
     }
 
-    /**
-     * A helper method to prevent you from having to hook into the event,
-     * use this to make your quads. (This always comes AFTER the textures are loaded)
-     *
-     * @param quadBakery     The QuadBakery.
-     * @param modelBakery
-     * @param templateBakery
-     */
     @Override
     public void registerModels(ElecQuadBakery quadBakery, ElecModelBakery modelBakery, ElecTemplateBakery templateBakery) {
         this.model = new TESRItemModel(new CraftingTableIVRenderer());
     }
 
-    /**
-     * A helper method to prevent you from having to hook into the event,
-     * use this to register your textures.
-     *
-     * @param textureMap The TextureMap.
-     */
     @Override
-    public void registerTextures(TextureMap textureMap) {
+    public void registerTextures(IIconRegistrar registrar) {
     }
 
 }
